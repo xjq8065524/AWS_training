a3fd6007b35f3103b2b810db715042b732120f30
Frank_xie <Junqing1014@gmail.com>
2025-07-01 15:30:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("user_interface.html")
 
 @app.route('/<path:filename>')
 def serve_file(filename):
